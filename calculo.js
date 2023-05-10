ad916d5e1cdc173e46caaa9f242b25d84960ad6c
jefersonlaurentino <128257769+jefersonlaurentino@users.noreply.github.com>
2023-05-10 16:36:49 -0300

    adicionado informações do imc

--- calculo.js
+++ calculo.js
@@ -16,18 +16,18 @@ function calcular() {
     var rkg2 = 24.9 * (a * a)
     var res2 = rkg2.toFixed(2) 
 
-    if (resimc < 18.5) {
+    if (resimc <= 18.5) {
         mos.innerHTML = `<span style="color: red;">${resimc}</span><br>você estar abaixo do peso!<br>seu peso ideal é entre<br> <strong>(${res1}Kg é ${res2}Kg)</strong>`
     } else if (resimc <= 24.9) {
-        mos.innerHTML = `<span style="color: green;">${resimc}</span><br>você estar com o peso normal!<br>seu peso ideal é entre<br> <strong>(${res1}Kg é ${res2}Kg)</strong>`
+        mos.innerHTML = `<span style="color: green;">${resimc}</span><br>você estar com o peso normal parabéns!<br>seu peso ideal é entre<br> <strong>(${res1}Kg é ${res2}Kg)</strong>`
     } else if (resimc <= 29.9) {
-        mos.innerHTML = `<span style="color: rgb(244, 208, 63);">${resimc}</span><br>você estar acima do peso!<br>seu peso ideal é entre<br> <strong>(${res1}Kg é ${res2}Kg)</strong>`
+        mos.innerHTML = `<span style="color: rgb(244, 208, 63);">${resimc}</span><br>você estar levemente acima do peso!<br>seu peso ideal é entre<br> <strong>(${res1}Kg é ${res2}Kg)</strong>`
     } else if (resimc <= 34.9) {
-        mos.innerHTML = `<span style="color: red;">${resimc}</span><br>você estar com obesidade (grau I)<br>seu peso ideal é entre<br> <strong>(${res1}Kg é ${res2}Kg)</strong>`
+        mos.innerHTML = `<span style="color: red;">${resimc}</span><br>você estar com obesidade (grau 1)<br>seu peso ideal é entre<br> <strong>(${res1}Kg é ${res2}Kg)</strong>`
     } else if (resimc <= 39.9) {
-        mos.innerHTML = `<span style="color: red;">${resimc}</span><br>você estar com obesidade (grau: II)<br>seu peso ideal é entre<br> <strong>(${res1}Kg é ${res2}Kg)</strong>`
+        mos.innerHTML = `<span style="color: red;">${resimc}</span><br>você estar com obesidade grau 2 (severa)<br>seu peso ideal é entre<br> <strong>(${res1}Kg é ${res2}Kg)</strong>`
     } else {
-        mos.innerHTML = `<span style="color: red;">${resimc}</span><br>você estar com obesidade (grau: III)<br>seu peso ideal é entre<br> <strong>(${res1}Kg é ${res2}Kg)</strong>`
+        mos.innerHTML = `<span style="color: red;">${resimc}</span><br>você estar com obesidade grau 3 (mórbida)<br>seu peso ideal é entre<br> <strong>(${res1}Kg é ${res2}Kg)</strong>`
     }
 }
 
